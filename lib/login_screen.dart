8af9874e88b611c0f86d69a54cd86613d7f7537e
Tu Nombre <hugogarsan@campus.monlau.com>
2024-04-23 20:19:01 +0200

    Funcionando apis de register y login con cambios de los tocones

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:BeatNow/UserSingleton.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
@@ -7,17 +6,10 @@ import 'auth_controller.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter/gestures.dart'; 
 
-class LoginScreen extends StatefulWidget {
-  @override
-  _LoginScreenState createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
+class LoginScreen extends StatelessWidget {
   final AuthController _authController = Get.find<AuthController>(); // Obtener instancia del controlador AuthController
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
-  bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -76,7 +68,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   children: [
                     TextField(
                       style: TextStyle(color: Colors.white),
-                      obscureText: _obscurePassword,
+                      obscureText: true,
                       decoration: InputDecoration(
                         hintText: 'Password',
                         hintStyle: TextStyle(color: Colors.white70),
@@ -85,16 +77,6 @@ class _LoginScreenState extends State<LoginScreen> {
                         border: OutlineInputBorder(
                           borderRadius: BorderRadius.circular(12.0),
                           borderSide: BorderSide.none,
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            _obscurePassword ? Icons.visibility_off : Icons.visibility,
-                            color: Colors.white,
-                          ),
-                          onPressed: () {
-                            // Cambiar el estado de _obscurePassword para alternar entre mostrar u ocultar la contraseña
-                            _togglePasswordVisibility();
-                          },
                         ),
                       ),
                       controller: _passwordController,
@@ -124,7 +106,6 @@ class _LoginScreenState extends State<LoginScreen> {
                   child: Text('Sign In'),
                   onPressed: () {
                     // Navega a la pestaña HomeScreenState
-                    //_authController.changeTab(3);
                     _login(_usernameController.text, _passwordController.text, context);
                   },
                   style: buttonStyle,
@@ -172,60 +153,55 @@ class _LoginScreenState extends State<LoginScreen> {
     );
   }
 
-  void _togglePasswordVisibility() {
-    // Cambiar el estado de _obscurePassword para alternar entre mostrar u ocultar la contraseña
-    setState(() {
-      _obscurePassword = !_obscurePassword;
-    });
+void _login(String username, String password, BuildContext context) async {
+  // Verificar si los campos de nombre de usuario y contraseña no están vacíos
+  if (username.isEmpty || password.isEmpty) {
+    _showErrorSnackBar('Username and password are required.', context);
+    return;
   }
 
+  try {
+    // Llamar a la función para iniciar sesión
+    final Map<String, dynamic> response = await loginUser(username, password);
 
-  void _login(String username, String password, BuildContext context) async {
-    // Verificar si los campos de nombre de usuario y contraseña no están vacíos
-    if (username.isEmpty || password.isEmpty) {
-      _showErrorSnackBar('Username and password are required.', context);
-      return;
+    // Verificar si la respuesta contiene la clave 'message' y su valor es 'ok'
+    if (response.containsKey('message') && response['message'] == 'ok') {
+      _showErrorSnackBar('Login successful!', context);
+      // Aquí puedes manejar la lógica para redirigir al usuario a otra pantalla o realizar otras acciones necesarias después del inicio de sesión exitoso
+      _authController.changeTab(3);
+    } else {
+      // Si la respuesta no contiene 'message' o el valor no es 'ok', mostrar un mensaje de error basado en el contenido de la respuesta
+      final errorMessage = response.containsKey('detail') ? response['detail'] : 'Unexpected response from server';
+      _showErrorSnackBar(errorMessage, context);
     }
-
-    try {
-      // Llamar a la función para iniciar sesión
-      final Map<String, dynamic> response = await loginUser(username, password);
-      
-      // Manejar la respuesta del servidor
-      if (response.containsKey('message')) {
-        // Si la respuesta contiene la clave 'message', el inicio de sesión fue exitoso
-        _showErrorSnackBar('Login successful!', context);
-        // Aquí puedes manejar la lógica para redirigir al usuario a otra pantalla o realizar otras acciones necesarias después del inicio de sesión exitoso
-        _authController.changeTab(3);
-      } else {
-        // Si la respuesta no contiene 'message', mostrar un mensaje de error basado en el detalle proporcionado por el servidor
-        _showErrorSnackBar(response['detail'], context);
-      }
-    } catch (e) {
-      // Capturar cualquier excepción que pueda ocurrir durante el proceso de inicio de sesión
-      _showErrorSnackBar('Failed to login: $e', context);
-    }
+  } catch (e) {
+    // Capturar cualquier excepción que pueda ocurrir durante el proceso de inicio de sesión
+    _showErrorSnackBar('Failed to login: $e', context);
   }
+}
 
-  Future<Map<String, dynamic>> loginUser(String username, String password) async {
-    Uri apiUrl = Uri.parse('http://217.182.70.161:6969/login');
 
-    Map<String, dynamic> body = {
-      'username': username,
-      'password': password,
-    };
 
-    final http.Response response = await http.post(
-      apiUrl,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-      body: jsonEncode(body),
-    );
 
-    // Decodificar y devolver la respuesta del servidor
-    return json.decode(response.body);
-  }
+Future<Map<String, dynamic>> loginUser(String username, String password) async {
+  final apiUrl = Uri.parse('http://217.182.70.161:6969/login');
+  
+  final body = {
+    'username': username,
+    'password': password,
+  };
+
+  final response = await http.post(
+    apiUrl,
+    headers: <String, String>{
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: body,
+  );
+
+  // Decodificar y devolver la respuesta del servidor
+  return json.decode(response.body);
+}
 
 
   void _showErrorSnackBar(String message, BuildContext context) {
@@ -239,35 +215,4 @@ class _LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
-
-Future<void> getUserInfo() async {
-  try {
-    // Define the API URL
-    final apiUrl = Uri.parse('http://217.182.70.161:8001/v1/api/users/users/me');
-    // Fetch user information from the API
-    final response = await http.get(apiUrl);
-
-    // Check if the response status code is 200 (success)
-    if (response.statusCode == 200) {
-      // Decode the response body from JSON
-      final userData = json.decode(response.body);
-
-      // Process the user data
-      processUserData(userData);
-    } else {
-      // If the response status code is not 200, throw an exception
-      throw Exception('Failed to fetch user information. Status code: ${response.statusCode}');
-    }
-  } catch (error) {
-    // Catch any errors that occur during the process
-    print('Error fetching user information: $error');
-  }
-}
-
-void processUserData(Map<String, dynamic> userData) {
-  // Add your logic to process the user data here
-  UserSingleton().name = userData['name'];
-  UserSingleton().username = userData['username'];
-  UserSingleton().email = userData['email'];
-}
 }
